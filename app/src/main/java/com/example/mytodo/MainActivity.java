bebe3d79e758cb76799e89e64653687469b610e9
Victoria Delali <victoria.ake@unionsg.com>
2021-10-26 12:53:47 +0000

    Register Page Completed

--- app/src/main/java/com/example/mytodo/MainActivity.java
+++ app/src/main/java/com/example/mytodo/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.mytodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
+    TextView fPassword,register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register=(TextView) findViewById(R.id.registerTBut);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        Intent loginIntent = new Intent(MainActivity.this, RegisterPage.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
